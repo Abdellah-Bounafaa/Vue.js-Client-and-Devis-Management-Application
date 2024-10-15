aa0f8235835ee970ec91430babc5a5c961a097cf
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2024-10-15 01:47:58 +0100

    Add store module for managing clients and client creation

---
+++ src/store.js
@@ -0,0 +1,45 @@
+// src/store.js
+import { reactive } from 'vue';
+import axiosInstance from './data/axiosInstace';
+
+const state = reactive({
+    clients: [],
+    showModal: false,
+    newClient: {
+        nom: '',
+        prenom: '',
+        date_naissance: '',
+        est_personne: false
+    }
+});
+
+const fetchClients = async () => {
+    try {
+        const response = await axiosInstance.get('/clients');
+        state.clients = response.data;
+    } catch (error) {
+        console.error('Error fetching clients:', error);
+    }
+};
+
+const addClient = async () => {
+    try {
+        const response = await axiosInstance.post('/clients/create', state.newClient);
+        state.clients.push(response.data);
+        state.showModal = false;
+        state.newClient = {
+            nom: '',
+            prenom: '',
+            date_naissance: '',
+            est_personne: false
+        };
+    } catch (error) {
+        console.error('Error adding client:', error);
+    }
+};
+
+export default {
+    state,
+    fetchClients,
+    addClient
+};
