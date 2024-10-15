c6f4ed09d5b6ce0a75c81ec35be647a62c8c34cf
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2024-10-15 01:48:52 +0100

    Refactor CSS layout to center content in ClientList.vue and update add button

--- src/views/ClientList.vue
+++ src/views/ClientList.vue
@@ -2,9 +2,9 @@
   <div class="client-list">
     <div class="header">
       <h2>Liste des clients :</h2>
-      <a href="#" class="add-client" @click="showModal = true">+</a>
+      <a href="#" class="add-btn" @click="store.state.showModal = true">+</a>
     </div>
-    <div v-for="(client, index) in clients" :key="index" class="client-card">
+    <div v-for="(client, index) in store.state.clients" :key="index" class="client-card">
       <div class="client-type">Particulier</div>
       <div class="client-info">
         <span class="client-name">{{ client.nom }} {{ client.prenom }}</span>
@@ -16,28 +16,28 @@
         {{ getStatusText(client.estPersonne) }}
       </div>
     </div>
-    <div v-if="showModal" class="modal" @click.self="showModal = false">
+    <div v-if="store.state.showModal" class="modal" @click.self="store.state.showModal = false">
       <div class="modal-content">
-        <span class="close" @click="showModal = false">&times;</span>
+        <span class="close" @click="store.state.showModal = false">&times;</span>
         <h2>Ajouter un client</h2>
-        <form @submit.prevent="addClient">
+        <form @submit.prevent="store.addClient">
           <div>
             <label for="nom">Nom:</label>
-            <input type="text" id="nom" v-model="newClient.nom" required>
+            <input type="text" id="nom" v-model="store.state.newClient.nom" required>
           </div>
           <div>
             <label for="prenom">Prénom:</label>
-            <input type="text" id="prenom" v-model="newClient.prenom" required>
+            <input type="text" id="prenom" v-model="store.state.newClient.prenom" required>
           </div>
           <div>
             <label for="dateNaissance">Date de naissance:</label>
-            <input type="date" id="date_naissance" v-model="newClient.date_naissance" required>
+            <input type="date" id="date_naissance" v-model="store.state.newClient.date_naissance" required>
           </div>
           <div>
             <label for="estPersonne">Est Personne:</label>
-            <input type="checkbox" id="est_personne" v-model="newClient.est_personne">
+            <input type="checkbox" id="est_personne" v-model="store.state.newClient.est_personne">
           </div>
-    <button type="submit" class="full-width-button">Ajouter</button>
+          <button type="submit" class="full-width-button">Ajouter</button>
         </form>
       </div>
     </div>
@@ -45,48 +45,13 @@
 </template>
 
 <script>
-import { reactive, toRefs, onMounted } from 'vue';
-import axiosInstance from '../data/axiosInstace';
+import '../assets/clients.css'
+import { onMounted } from 'vue';
+import store from '../store';
 import { format } from 'date-fns';
 
 export default {
   setup() {
-    const state = reactive({
-      clients: [],
-      showModal: false,
-      newClient: {
-        nom: '',
-        prenom: '',
-        date_naissance: '',
-        est_personne: false
-      }
-    });
-
-    const fetchClients = async () => {
-      try {
-        const response = await axiosInstance.get('/clients');
-        state.clients = response.data;
-      } catch (error) {
-        console.error('Error fetching clients:', error);
-      }
-    };
-
-    const addClient = async () => {
-      try {
-        const response = await axiosInstance.post('/clients/create', state.newClient);
-        state.clients.push(response.data);
-        state.showModal = false;
-        state.newClient = {
-          nom: '',
-          prenom: '',
-          date_naissance: '',
-          est_personne: false
-        };
-      } catch (error) {
-        console.error('Error adding client:', error);
-      }
-    };
-
     const formatDate = (dateString) => {
       return format(new Date(dateString), 'dd MMMM yyyy');
     };
@@ -100,171 +65,15 @@ export default {
     };
 
     onMounted(() => {
-      fetchClients();
+      store.fetchClients();
     });
 
     return {
-      ...toRefs(state),
+      store,
       formatDate,
       getStatusClass,
-      getStatusText,
-      addClient
+      getStatusText
     };
   }
 };
 </script>
-
-<style scoped>
-.client-list {
-  background-color: #1c0833;
-  padding: 30px;
-  color: #ffffff;
-  border-radius: 8px;
-  width: 800px;
-  margin: 0 auto; /* Center the client list */
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-button {
-  background-color: #5a2b8a;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 24px; /* Increase font size for the + symbol */
-}
-
-button:hover {
-  background-color: #4a2370;
-}
-
-.client-card {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #2b0d45;
-  border-radius: 8px;
-  padding: 10px 30px;
-  margin-bottom: 10px;
-  gap: 20px; /* Increase space between elements */
-}
-
-.client-type {
-  background-color: #5a2b8a;
-  padding: 5px 10px;
-  border-radius: 4px;
-  color: #fff;
-}
-
-.client-info {
-  flex-grow: 1;
-  margin-left: 20px;
-  color: #fff;
-}
-
-.client-name,
-.client-email {
-  display: block;
-}
-
-.status-active {
-  color: #00ff00;
-}
-
-.status-prospect {
-  color: #ff7b00;
-}
-
-.status-resolved {
-  color: #ff005c;
-}
-
-.modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal-content {
-  background-color: #2b0d45;
-  padding: 20px;
-  border-radius: 8px;
-  width: 400px;
-  color: #fff;
-}
-
-.close {
-  float: right;
-  font-size: 24px;
-  cursor: pointer;
-}
-
-form div {
-  margin-bottom: 10px;
-}
-
-form label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-form input[type="text"],
-form input[type="date"] {
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-}
-
-form input[type="checkbox"] {
-  margin-right: 10px;
-}
-
-form button {
-  background-color: #5a2b8a;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-form button:hover {
-  background-color: #4a2370;
-}
-
-.full-width-button {
-  width: 100%; /* Make the button full width */
-  padding: 8px; /* Decrease padding */
-}
-.add-client {
-  background-color: #5a2b8a;
-  color: #fff;
-  border: none;
-  padding: 10px;
-  border-radius: 8px;
-  cursor: pointer;
-  font-size: 24px;
-  text-align: center;
-  line-height: 24px; /* Center the + symbol vertically */
-  width: 30px; /* Make the button larger */
-  height: 30px; /* Make the button larger */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-</style>
